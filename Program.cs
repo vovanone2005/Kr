25376d021fda5c46cf8f0e8a134d08d58d2625f8
vovanone2005 <voevoda.junior@mail.ru>
2022-10-01 17:02:34 +0300

    save_2

--- Program.cs
+++ Program.cs
@@ -12,3 +12,13 @@ void SecondArrayWithIF(string[] array1, string[] array2)
         }
     }
 }
+void PrintArray(string[] array)
+{
+    for (int i = 0; i < array.Length; i++)
+    {
+        Console.Write($"{array[i]} ");
+    }
+    Console.WriteLine();
+}
+SecondArrayWithIF(array1, array2);
+PrintArray(array2);
